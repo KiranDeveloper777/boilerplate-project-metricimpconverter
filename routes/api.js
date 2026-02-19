aa0b2d7a42073f786bcd6c071052adb0536e4de4
KiranDeveloper777 <kirant200513@gmail.com>
2026-02-19 20:42:51 +0530

    Add API endpoint for unit conversion

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,40 @@
 'use strict';
 
-const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
   let convertHandler = new ConvertHandler();
+
+  app.route('/api/convert')
+    .get(function (req, res){
+
+      let input = req.query.input;
+
+      let initNum = convertHandler.getNum(input);
+      let initUnit = convertHandler.getUnit(input);
+
+      if (initNum === "invalid number" && initUnit === "invalid unit")
+        return res.json("invalid number and unit");
+
+      if (initNum === "invalid number")
+        return res.json("invalid number");
+
+      if (initUnit === "invalid unit")
+        return res.json("invalid unit");
+
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+      res.json({
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string
+      });
+
+    });
 
 };
